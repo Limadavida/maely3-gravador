2ec3c6c235512ab00542e902608e1d447a90e20e
David Lima <limadavidnascimento@gmail.com>
2021-11-19 15:52:07 -0300

    80% consertando Bug

--- videorecorder.py
+++ videorecorder.py
@@ -4,40 +4,14 @@
 #5 se audit==true, recorte videos salvos e coloque em outra pasta
 
 ### video recorder function ###
-def write_video(file_path, frames, fps):
-    """
-    Writes frames to an mp4 video file
-    :param file_path: Path to output video, must end with .mp4
-    :param frames: List of PIL.Image objects
-    :param fps: Desired frame rate
-    """
 
-    #w, h = frames[0].size
-    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-    #writer = cv.VideoWriter(file_path, fourcc, fps, (w, h))
-    writer = cv2.VideoWriter(file_path, fourcc, fps, (600, 600))
 
-    for frame in frames:
-        writer.write(pil_to_cv(frame))
-
-    writer.release() 
-
-def initializeVideoWriter(dirvideosave, video_width, video_height, videoStream, fps):
-    #outputVideoPath = "audit/auditoria.avi"
-    #outputVideoPath = f"audit/auditoria{number_random}.avi"
-    #outputVideoPath = dirvideosave + ".mp4"
-    outputVideoPath = "videos/1" + ".avi"
-
-    #sourceVideofps = videoStream.get(cv2.CAP_PROP_FPS)
-    sourceVideofps = fps
-    fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-    return cv2.VideoWriter(outputVideoPath, fourcc, sourceVideofps,
-                           (video_width, video_height), True)
-
+### time ###
 def timenow():
     from datetime import datetime
     date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return date_time
+
 
 
 ### read config file ###
@@ -57,6 +31,7 @@ sensory = parser.getint("sensor", "y")
 sensorh = parser.getint("sensor", "h")
 sensorx = parser.getint("sensor", "x")
 sensorw = parser.getint("sensor", "w")
+
 
 ### relative path ###
 import os
@@ -85,22 +60,48 @@ else:
     cam = str(cam)
 
 cap = cv2.VideoCapture(cam)
-     
+
 if (cap.isOpened()== False): 
   print("Error opening video stream or file")
+
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+frame_size = (frame_width,frame_height)
+
+sensor_width = sensory+sensorh
+sensor_height = sensorx+sensorw
+sensor_size = (sensor_width, sensor_height)
+
+out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, sensor_size)
+
+cap.set(3,300)
+cap.set(4,300)
+cap.set(15, 0.1)
+
+print("width={}".format(cap.get(3)))
+print("height={}".format(cap.get(4)))
+print("exposure={}".format(cap.get(15)))
+
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+print(fps)
 
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
-        width = sensory+sensorh
-        height = sensorx+sensorw
-        videocrop = frame[sensory:width, sensorx:height]
+        #sensor_width = sensory+sensorh
+        #sensor_height = sensorx+sensorw
+        videocrop = frame[sensory:sensor_width, sensorx:sensor_height]
+
+        out.write(videocrop)
+
 
         if not private:
             print("running in private_mode")
             cv2.imshow('original', frame) 
             cv2.imshow('sensor', videocrop) 
-          
+        
+
         if cv2.waitKey(delayfps) == 27:
             break
 
@@ -108,14 +109,22 @@ while(cap.isOpened()):
         dirvideosave = dir_videosave + f"/{now}"
         print(dirvideosave)
         #write_video(dirvideosave, dirvideosave, videocrop, videowriterfps)
-        print(width)
-        initializeVideoWriter(dirvideosave=dirvideosave, video_width=width, video_height=height, videoStream=videocrop, fps=videowriterfps)
+        #initializeVideoWriter(dirvideosave=dirvideosave, video_width=width, video_height=height, videoStream=frame, fps=fps)
+        outputVideoPath = "videos/1" + ".mp4"
+
+        #sourceVideofps = videoStream.get(cv2.CAP_PROP_FPS)
+        sourceVideofps = fps
+        fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+
+        #cv2.VideoWriter("videos/2" + ".mp4", fourcc, fps, frame_size, True)
     
+
     else: 
         print(f"Break the loop if ret is false, ret={ret}")
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
 
 
@@ -128,6 +137,13 @@ def initializeVideoWriter(video_width, video_height, videoStream):
     return cv2.VideoWriter(outputVideoPath, fourcc, sourceVideofps,
                            (video_width, video_height), True)
 
+
+    #outputVideoPath = "audit/auditoria.avi"
+    outputVideoPath = f"audit/auditoria{number_random}.avi"
+    sourceVideofps = videoStream.get(cv2.CAP_PROP_FPS)
+    fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+    return cv2.VideoWriter(outputVideoPath, fourcc, sourceVideofps,
+                           (video_width, video_height), True)
 -----
 
 def write_video(file_path, frames, fps):
